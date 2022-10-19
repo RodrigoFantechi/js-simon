144f5c1b51d18c4b086402e57b25df91a17f1f7a
RodrigoFantechi <rodrifante595@libero.it>
2022-10-19 15:12:29 +0200

    finish exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,66 @@
 /* Descrizione:
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+
+
+
+const container = document.querySelector('.container');
+const result = document.querySelector('.result');
+const arrayNumeriRandom = generaArrayNumeriRandomici();
+
+generaGriglia(container, arrayNumeriRandom);
+startGame(arrayNumeriRandom, result, container);
+
+
+
+function generaGriglia(whereGemerateGrid, arrayRandomNumber) {
+
+    for (let i = 0; i < arrayRandomNumber.length; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.innerText = arrayRandomNumber[i];
+        whereGemerateGrid.insertAdjacentElement('beforeend', square);
+    }
+};
+
+function generaArrayNumeriRandomici() {
+    const array = [];
+    while (array.length !== 5) {
+        const number = Math.floor(Math.random() * (100 - 1) + 1) + 1;
+        if (!array.includes(number)) {
+            array.push(number);
+        }
+    }
+    return array;
+}
+
+function startGame(arrayRandomNumber, wherePrintResult, whereGemerateGrid ) {
+
+
+    setTimeout(function () {
+        whereGemerateGrid.style.display = 'none';
+    }, 3000)
+
+    setTimeout(function () {
+
+        let numeriIndovinati = [];
+        for (let i = 1; i <= arrayRandomNumber.length; i++) {
+            let numeroInseritoDallUser = Number(prompt(`inserisci il ${i}° numero`))
+            if (numeroInseritoDallUser === arrayRandomNumber[i - 1]) {
+                numeriIndovinati.push(numeroInseritoDallUser);
+            }
+        }
+        whereGemerateGrid.style.display = 'flex';
+        wherePrintResult.style.display ='block';
+        if(numeriIndovinati.length ===0){
+        wherePrintResult.innerText = `Hai indovinato 0 numeri.`;
+        } else if(numeriIndovinati.length ===0){
+            wherePrintResult.innerText = `Hai indovinato 1 numero. Il numero: ${numeriIndovinati}`;
+        }else{
+            wherePrintResult.innerText = `Hai indovinato ${numeriIndovinati.length} numeri. I numeri: ${numeriIndovinati}`;
+        }
+    }, 3100)
+
+
+
+}
